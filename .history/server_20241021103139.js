ee4cbd661a1b9473b1df9aa3ab04465e423fb3e8
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-10-21 11:23:03 -0400

    first commit

---
+++ .history/server_20241021103139.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3004;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/whoamis', (req, res) => {
+    const ipaddress = req.ip;
+    const language = req.headers['accept-language'];
+    const software = req.headers['user-agent'];
+
+    res.json({
+        ipaddress: ipaddress,
+        language: language,
+        software: software,
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
